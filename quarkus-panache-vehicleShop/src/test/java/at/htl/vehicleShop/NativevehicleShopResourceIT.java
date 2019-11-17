711cfc1180a20eaca46476461f9ab7826e3f7c22
Nenad TRIPIC <tripicnenad@hotmail.com>
2019-11-17 15:41:08 +0100

    Created Project

---
+++ quarkus-panache-vehicleShop/src/test/java/at/htl/vehicleShop/NativevehicleShopResourceIT.java
@@ -0,0 +1,9 @@
+package at.htl.vehicleShop;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativevehicleShopResourceIT extends vehicleShopResourceTest {
+
+    // Execute the same tests but in native mode.
+}
