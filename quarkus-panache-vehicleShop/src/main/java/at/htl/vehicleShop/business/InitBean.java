906180a954a1eb85c8b7ce4b7fe55aeb1f9125e5
Nenad TRIPIC <tripicnenad@hotmail.com>
2019-11-18 18:35:57 +0100

    Code refactoring, removed unnecessary things

--- quarkus-panache-vehicleShop/src/main/java/at/htl/vehicleShop/business/InitBean.java
+++ quarkus-panache-vehicleShop/src/main/java/at/htl/vehicleShop/business/InitBean.java
@@ -8,15 +8,10 @@ import io.quarkus.runtime.StartupEvent;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class InitBean {
-
-    @Inject
-    EntityManager em;
 
     @Transactional
     void init(@Observes StartupEvent ev) {
