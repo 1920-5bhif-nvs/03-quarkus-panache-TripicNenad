b4014d17f87f1575569059fb500e78fd09c58075
Nenad TRIPIC <tripicnenad@hotmail.com>
2019-11-30 14:40:34 +0100

    Added docker-compose for production. Project can run now in docker

--- quarkus-hibernate-vehicleShop/src/test/java/at/htl/vehicleShop/NativevehicleShopResourceIT.java
+++ quarkus-hibernate-vehicleShop/src/test/java/at/htl/vehicleShop/NativevehicleShopResourceIT.java
@@ -1,8 +1,6 @@
 package at.htl.vehicleShop;
 
-import io.quarkus.test.junit.SubstrateTest;
 
-@SubstrateTest
 public class NativevehicleShopResourceIT extends vehicleShopResourceTest {
 
     // Execute the same tests but in native mode.
